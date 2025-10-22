b513a3dfcd5bc496f935895b1a0b16846f048674
Lukasz Gornicki <lpgornicki@gmail.com>
2025-10-22 11:12:57 +0200

    feat: improve lighthouse throttling settings for more realistic testing

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,14 +1,14 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['http://localhost:3000/'],
-      numberOfRuns: 1,
+      url: ['http://localhost:3000/', 'http://localhost:3000/success-stories', 'http://localhost:3000/contact'],
+      numberOfRuns: 3,
       settings: {
         formFactor: 'desktop',
         throttling: {
           rttMs: 40,
           throughputKbps: 10240,
-          cpuSlowdownMultiplier: 1,
+          cpuSlowdownMultiplier: 2, // Simulate slower CPU (was 1, now 2x slowdown)
         },
         onlyCategories: [
           'performance',
