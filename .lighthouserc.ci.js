1101e823b89ce3e5d20b2d3d0f9169c0ce1ef95d
Lukasz Gornicki <lpgornicki@gmail.com>
2025-11-10 13:23:03 +0100

    ci: add missing URL config to Lighthouse CI files (#174)

--- .lighthouserc.ci.js
+++ .lighthouserc.ci.js
@@ -1,6 +1,9 @@
+const urls = require('./lighthouse.urls.js')
+
 module.exports = {
   ci: {
     collect: {
+      url: urls,
       settings: {
         configPath: './lighthouserc.js',
         // skip seo crawlable in CI as crawlability is blocked on Vercel preview deployments
--- lighthouse.mobile.config.ci.js
+++ lighthouse.mobile.config.ci.js
@@ -1,6 +1,9 @@
+const urls = require('./lighthouse.urls.js')
+
 module.exports = {
   ci: {
     collect: {
+      url: urls,
       settings: {
         configPath: './lighthouse.mobile.config.js',
         // skip seo crawlable in CI as crawlability is blocked on Vercel preview deployments
